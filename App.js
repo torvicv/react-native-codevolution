70c517a71ee63098a98c4465c2270bff06b8becb
victor <torvicv@gmail.com>
2023-10-28 19:32:36 +0200

    video 13

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { View, Text, Image, ImageBackground, ScrollView, Button } from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "yellow", padding: 60 }}>
-      <Button title="Press" onPress={() => console.log("Button pressed")}
-        color="midnightblue" disabled />
+      <Pressable  onPress={() => console.log("Image pressed")} style={{ width: 300, height: 300}} >
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
     </View>
   );
 }
