79d059e9c2e5ad4061f8953ed184fa86f008eabc
victor <torvicv@gmail.com>
2023-10-28 14:05:13 +0200

    video 10

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
-
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
-  return <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-    <Text>
-      <Text style={{ color: 'white' }}>Hello</Text> world</Text>
+  return <View style={{ flex: 1, backgroundColor: "yellow", padding: 60 }}>
+    {/*<Image source={logoImg} style={{ width: 300, height: 300 }} />
+    <Image source={{ uri: "https://cdn.pixabay.com/photo/2018/11/17/22/15/trees-3822149_1280.jpg" }} style={{ width: 300, height: 300 }} />*/}
+    <ImageBackground source={logoImg} style={{ flex: 1 }} >
+      <Text>Image Text</Text>
+    </ImageBackground>
   </View>
 }
