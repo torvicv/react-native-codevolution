12391ff890335e5c3c6f856f27a23546a06b6631
victor <torvicv@gmail.com>
2023-10-29 11:31:20 +0100

    video 15

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal } from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar } from "react-native";
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
@@ -7,17 +7,10 @@ export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "yellow", padding: 60 }}>
-      <Button title="Press" onPress={() => setIsModalVisible(true)}
-        color="midnightblue" />
-      <Modal visible={isModalVisible} onRequestClose={() => setIsModalVisible(false)} 
-        animationType="slide" presentationStyle="pageSheet">
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Modal content</Text>
-          <Button title="Close" color="midnightblue"
-            onPress={() => setIsModalVisible(false)} />
-        </View>
-      </Modal>
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <StatusBar barStyle='light-content' backgroundColor='lightgreen'>
+        <Text>Hello StatusBar</Text>
+      </StatusBar>
     </View>
   );
 }
