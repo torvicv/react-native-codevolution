d71336b243c3c510eaae3caa125e3bb47b374c90
victor <torvicv@gmail.com>
2023-10-29 18:18:31 +0100

    video 21

--- App.js
+++ App.js
@@ -7,12 +7,28 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.lightblueBg, styles.box]} >
+        <Text style={styles.title}>StyleSheet API</Text>
+      </View>
+      <View style={[styles.lightgreenBg, styles.box]} >
+        <Text style={styles.title}>StyleSheet API</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: 'plum', padding: 60 },
-  title: { fontSize: 20 }
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+    backgroundColor: 'pink'
+  },
+  lightblueBg: {
+    backgroundColor: 'lightblue'
+  },
+  lightgreenBg: {
+    backgroundColor: 'lightgreen'
+  }
 })
