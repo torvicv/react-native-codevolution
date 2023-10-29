5976f46e9cb2c263637959a91937df9b132e5d73
victor <torvicv@gmail.com>
2023-10-29 11:47:55 +0100

    video 17

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator } from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
 const logoImg = require('./assets/adaptive-icon.png');
 
 export default function App() {
@@ -8,8 +8,18 @@ export default function App() {
 
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ActivityIndicator size='large' />
-      <ActivityIndicator size='large' color='midnightblue' animating={false} /> 
+      <Button onPress={() => Alert.alert('Invalid data')} title="Alert" />
+      <Button title="Alert 2" onPress={() => Alert.alert("Invalid data", "DOB Incorrect", [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel pressed')
+        },
+        {
+          text: 'OK',
+          onPress: () => console.log('OK pressed')
+        }
+      ])
+    } />
     </View>
   );
 }
